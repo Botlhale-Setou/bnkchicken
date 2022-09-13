3517d4b397f5afc49f23de90cca32b943359d89a
Botlhale Setou <botlhale.setou@gmail.com>
2022-09-13 19:31:10 +0200

    Complete contact section, slider2 and menu section

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 const btnNo = document.querySelector('#no');
+const bntYes = document.querySelector('#yes');
 const slider = document.querySelector('#slider');
+const slider2 = document.querySelector('#slider2');
 const btnBurger = document.querySelector('#burgerbtn');
 const menu = document.querySelector('#menu');
+const contact = document.querySelector('#main-contact');
+const minigame = document.querySelector('#minigame');
+const btnX = document.querySelector('#btnX');
 
 let indexSlider = 1;
+let indexSlider2 = 1;
 let active = false;
 
 btnBurger.addEventListener('click', () => {
@@ -19,7 +25,7 @@ btnBurger.addEventListener('click', () => {
 });
 
 btnNo.addEventListener('click', () => {
-  if (indexSlider === 6) {
+  if (indexSlider === 7) {
     indexSlider = 1;
   } else {
     indexSlider += 1;
@@ -43,7 +49,59 @@ btnNo.addEventListener('click', () => {
     case 6:
       slider.style.left = '-500vw';
       break;
+    case 7:
+      slider.style.left = '-600vw';
+      break;
     default:
       break;
   }
+  if (indexSlider2 === 10) {
+    indexSlider2 = 1;
+  } else {
+    indexSlider2 += 1;
+  }
+  switch (indexSlider2) {
+    case 1:
+      slider2.style.left = 0;
+      break;
+    case 2:
+      slider2.style.left = '-100vw';
+      break;
+    case 3:
+      slider2.style.left = '-200vw';
+      break;
+    case 4:
+      slider2.style.left = '-300vw';
+      break;
+    case 5:
+      slider2.style.left = '-400vw';
+      break;
+    case 6:
+      slider2.style.left = '-500vw';
+      break;
+    case 7:
+      slider2.style.left = '-600vw';
+      break;
+    case 8:
+      slider2.style.left = '-700vw';
+      break;
+    case 9:
+      slider2.style.left = '-800vw';
+      break;
+    case 10:
+      slider2.style.left = '-900vw';
+      break;
+    default:
+      break;
+  }
+});
+
+bntYes.addEventListener('click', () => {
+  contact.style.left = '10vw';
+  minigame.style.right = '-100vw';
+});
+
+btnX.addEventListener('click', () => {
+  contact.style.left = '-80vw';
+  minigame.style.right = 0;
 });
